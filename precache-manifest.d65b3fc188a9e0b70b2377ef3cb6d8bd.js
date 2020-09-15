20239c6d54060477e82acfba6bb9274a5a828f32
velmor99 <volgagaz24100@gmail.com>
2020-09-15 21:44:49 +0300

    Updates

---
+++ precache-manifest.d65b3fc188a9e0b70b2377ef3cb6d8bd.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e08b544c3a982c462efa6c0cff837d",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "8955f2258fe93ee4c0ad",
+    "url": "/goit-react-hw-08-phonebook/static/css/3.936e6350.chunk.css"
+  },
+  {
+    "revision": "fff40e4343826c569b87",
+    "url": "/goit-react-hw-08-phonebook/static/css/4.0c2e362b.chunk.css"
+  },
+  {
+    "revision": "9c81888a350df3f6d4de",
+    "url": "/goit-react-hw-08-phonebook/static/css/5.aa303074.chunk.css"
+  },
+  {
+    "revision": "7f981028b7595ca30b98",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.586e2f88.chunk.css"
+  },
+  {
+    "revision": "8985c31c9b41bbff5d92",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.bfe92665.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.bfe92665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8955f2258fe93ee4c0ad",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.ae506591.chunk.js"
+  },
+  {
+    "revision": "fff40e4343826c569b87",
+    "url": "/goit-react-hw-08-phonebook/static/js/4.1c835ab0.chunk.js"
+  },
+  {
+    "revision": "9c81888a350df3f6d4de",
+    "url": "/goit-react-hw-08-phonebook/static/js/5.f4c1a2c4.chunk.js"
+  },
+  {
+    "revision": "51171dcd31ce20a621c4",
+    "url": "/goit-react-hw-08-phonebook/static/js/6.a9e0d2e8.chunk.js"
+  },
+  {
+    "revision": "7f981028b7595ca30b98",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.310e94f7.chunk.js"
+  },
+  {
+    "revision": "1d74553c53383a2bf19e",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.d3d101e8.js"
+  },
+  {
+    "revision": "11f99e148911bea7cc317913f57f1252",
+    "url": "/goit-react-hw-08-phonebook/static/media/arya-v6-latin-regular.11f99e14.woff2"
+  },
+  {
+    "revision": "4847cddfa1eaedd261c0b6fdbefbf3da",
+    "url": "/goit-react-hw-08-phonebook/static/media/arya-v6-latin-regular.4847cddf.woff"
+  },
+  {
+    "revision": "3d4468305bd4270b4792acb13fbd6478",
+    "url": "/goit-react-hw-08-phonebook/static/media/donegal-one-v8-latin-regular.3d446830.woff2"
+  },
+  {
+    "revision": "c70929193660f895e08f26c6ebc8a5ee",
+    "url": "/goit-react-hw-08-phonebook/static/media/donegal-one-v8-latin-regular.c7092919.woff"
+  },
+  {
+    "revision": "15431381890720a5a4b62b33c8ae06c5",
+    "url": "/goit-react-hw-08-phonebook/static/media/roboto-v20-latin-ext_latin-500.15431381.woff2"
+  },
+  {
+    "revision": "3d9446f210892af971a3854d0f984ae9",
+    "url": "/goit-react-hw-08-phonebook/static/media/roboto-v20-latin-ext_latin-500.3d9446f2.woff"
+  },
+  {
+    "revision": "52bb58c8cb04cf3eea4f9ac0afa1d1f6",
+    "url": "/goit-react-hw-08-phonebook/static/media/roboto-v20-latin-ext_latin-700.52bb58c8.woff2"
+  },
+  {
+    "revision": "d0cc855e64aca1072711f49d1a38ca0c",
+    "url": "/goit-react-hw-08-phonebook/static/media/roboto-v20-latin-ext_latin-700.d0cc855e.woff"
+  },
+  {
+    "revision": "51db4691b982b906d8cd9de2e2c230fd",
+    "url": "/goit-react-hw-08-phonebook/static/media/roboto-v20-latin-ext_latin-900.51db4691.woff"
+  },
+  {
+    "revision": "d8e97377c715fe514986481ed626cd01",
+    "url": "/goit-react-hw-08-phonebook/static/media/roboto-v20-latin-ext_latin-900.d8e97377.woff2"
+  },
+  {
+    "revision": "c5bf51b68dc9fd7fe944d8947fe12518",
+    "url": "/goit-react-hw-08-phonebook/static/media/roboto-v20-latin-ext_latin-regular.c5bf51b6.woff2"
+  },
+  {
+    "revision": "d679a90608a65cc9394f67448ca2094a",
+    "url": "/goit-react-hw-08-phonebook/static/media/roboto-v20-latin-ext_latin-regular.d679a906.woff"
+  }
+]);
